d0df52a2eb0af65ea43deb9c1276ea110bd0be14
Tom Gowans <tomgowans@users.noreply.github.com>
2019-12-06 23:11:21 +0000

    Removed mock data

--- index.js
+++ index.js
@@ -50,98 +50,6 @@ const typeDefs = gql`
   }
 `;
 
-const departures = {
-  GetDepartureBoardResponse: [
-    {
-      $: {
-        xmlns: "http://thalesgroup.com/RTTI/2017-10-01/ldb/"
-      },
-      GetStationBoardResult: [
-        {
-          $: {
-            "xmlns:lt": "http://thalesgroup.com/RTTI/2012-01-13/ldb/types",
-            "xmlns:lt2": "http://thalesgroup.com/RTTI/2014-02-20/ldb/types",
-            "xmlns:lt3": "http://thalesgroup.com/RTTI/2015-05-14/ldb/types",
-            "xmlns:lt4": "http://thalesgroup.com/RTTI/2015-11-27/ldb/types",
-            "xmlns:lt5": "http://thalesgroup.com/RTTI/2016-02-16/ldb/types",
-            "xmlns:lt6": "http://thalesgroup.com/RTTI/2017-02-02/ldb/types",
-            "xmlns:lt7": "http://thalesgroup.com/RTTI/2017-10-01/ldb/types"
-          },
-          crs: ["ECR"],
-          generatedAt: ["2019-11-20T21:09:59.5101527+00:00"],
-          locationName: ["East Croydon"],
-          platformAvailable: ["true"],
-          trainServices: [
-            {
-              service: [
-                {
-                  destination: [
-                    {
-                      location: [
-                        {
-                          crs: ["GTW"],
-                          locationName: ["Gatwick Airport"]
-                        }
-                      ]
-                    }
-                  ],
-                  etd: ["On time"],
-                  length: ["12"],
-                  operator: ["Thameslink"],
-                  operatorCode: ["TL"],
-                  origin: [
-                    {
-                      location: [
-                        {
-                          crs: ["BDM"],
-                          locationName: ["Bedford"]
-                        }
-                      ]
-                    }
-                  ],
-                  platform: ["6"],
-                  serviceID: ["8NvDNS9Gpylp0MOg/MmCxw=="],
-                  serviceType: ["train"],
-                  std: ["21:07"]
-                },
-                {
-                  destination: [
-                    {
-                      location: [
-                        {
-                          crs: ["TBD"],
-                          locationName: ["Three Bridges"]
-                        }
-                      ]
-                    }
-                  ],
-                  etd: ["On time"],
-                  operator: ["Thameslink"],
-                  operatorCode: ["TL"],
-                  origin: [
-                    {
-                      location: [
-                        {
-                          crs: ["LBG"],
-                          locationName: ["London Bridge"]
-                        }
-                      ]
-                    }
-                  ],
-                  platform: ["3"],
-                  serviceID: ["rkEPUnFHelVWfAllpJoHZA=="],
-                  serviceType: ["train"],
-                  std: ["21:08"]
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
-
 const resolvers = {
   Query: {
     GetStationBoardResult(obj, { crs }) {
@@ -161,5 +69,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+  console.log(`🚂 Server ready at ${url}`);
 });
