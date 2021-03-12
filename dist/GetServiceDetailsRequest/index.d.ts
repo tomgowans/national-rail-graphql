9a851a0746bba77b489681bd78eb9319f51d497b
Tom Gowans <tomgowans@users.noreply.github.com>
2021-03-12 12:34:14 +0000

    remove /dist/

--- dist/GetServiceDetailsRequest/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Result, ServiceAttributes } from '../types';
-/**
- *
- * @param {string} serviceID (string): The LDBWS service ID of the service to request the details of. The service ID is obtained from a service listed in a StationBoard object returned from any other request.
- */
-declare function GetServiceDetailsRequest({ serviceID }: ServiceAttributes, tokenValue: string): Promise<Result>;
-export default GetServiceDetailsRequest;
