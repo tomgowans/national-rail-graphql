fb936e5ac5e6696342296413ef274631b2c0c02c
Tom Gowans <tomgowans@users.noreply.github.com>
2021-03-12 11:27:17 +0000

    Deleted dist

--- dist/GetArrBoardWithDetailsRequest/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Attributes } from '../types';
-declare function GetArrBoardWithDetailsRequest({ numRows, crs, filterCrs, filterType, timeOffset, timeWindow }: Attributes, tokenValue: string): Promise<unknown>;
-export default GetArrBoardWithDetailsRequest;
